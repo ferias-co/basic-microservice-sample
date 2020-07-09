b4bc640f52e7626a35922af3c3efa90de9e54929
Camilo Teixeira de Melo <camilotxm@outlook.com>
2020-07-09 12:37:52 -0300

    Insersão de testes de integração

---
+++ test/WebApplication.IntegrationTests/StartupSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication.IntegrationTests
+{
+    public static class StartupSettings
+    {
+        public static void Environment()
+        {
+            System.Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Sandbox");
+            System.Environment.SetEnvironmentVariable("CONNECTION_STRING", "mongodb://localhost:27017");
+            System.Environment.SetEnvironmentVariable("SUPPLIER_DATABASE", "suppliers");
+            System.Environment.SetEnvironmentVariable("SUPPLIER_REPOSITORY", "suppliers");
+        }
+    }
+}
--- test/WebApplication.IntegrationTests/WebApplicationTests.cs
+++ test/WebApplication.IntegrationTests/WebApplicationTests.cs
@@ -1,45 +1,101 @@
-using AngleSharp.Common;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using System.Net;
+using Xunit;
+
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace WebApplication.IntegrationTests
 {
-    public class WebApplicationTests : IClassFixture< WebApplicationFactory<WebApplication.Startup> >
+    public class WebApplicationTests : IClassFixture< WebApplicationFactory<Startup> >
     {
-        private readonly WebApplicationFactory<WebApplication.Startup> _factory;
 
 
-        public WebApplicationTests(WebApplicationFactory<WebApplication.Startup> factory)
+        public HttpClient Client { get; }
+
+        public WebApplicationTests(WebApplicationFactory<Startup> factory)
         {
-
-            System.Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Sandbox");
-            _factory = factory;
+            StartupSettings.Environment();
+            Client = factory.CreateClient();
         }
 
         [Fact]
-        public void Post_NewSupplier_And_Return_Successful()
+        public void CreateNewSupplier_PostRequest_Return_StatusCode_And_ApplicationJson() 
         {
             // Arrange
-            var requestUri = "/";
-            var payload = "{\"companyName\":\"Geraldo e Milena Entulhos Ltda\",\"enterpriseRegistry\":\"01.104.238/0001-20\"}";
+            var requestedUri = "/";
+            var payload = "{ \"companyName\":\"Geraldo e Milena Entulhos Ltda\","+
+                            "\"enterpriseRegistry\":\"01.104.238/0001-20\" }";
 
-            // Arrange
-            var client  = _factory.CreateClient();
-            HttpContent content = new StringContent(payload, Encoding.UTF8, "application/json");
-            var response = Task.Run(() => client.PostAsync(requestUri, content));
-            response.Wait();
-            
-            // Assert
+            // Act
+            Task<HttpResponseMessage> response = PostRequest(requestedUri, payload);
+
+            //Assert
             Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode);
             Assert.Equal("application/json; charset=utf-8",
                 response.Result.Content.Headers.ContentType.ToString());
         }
 
+        [Fact]
+        public void QuerySupplier_GetRequest_Return_StatusCode_And_ApplicationJson()
+        {
+            // Arrange
+            var payload = "{ \"companyName\":\"Milho Lactinios Ltda\"," +
+                            "\"enterpriseRegistry\":\"01.103.238/0001-21\" }";
+
+            Task<HttpResponseMessage> postResponse = PostRequest("/", payload);
+            dynamic jObj = DeserializePostResponse(postResponse);
+
+            // Act
+            Task<HttpResponseMessage> response = GetRequest("/" + jObj["id"]);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.Result.StatusCode);
+            Assert.Equal("application/json; charset=utf-8",
+                response.Result.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public void CreateNewSupplierInvalid_PostRequest_Return_BadRequest()
+        {
+            // Arrange
+            var payload = "{ }";
+            Task<HttpResponseMessage> response = PostRequest("/", payload);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.Result.StatusCode);
+        }
+
+
+
+        private static dynamic DeserializePostResponse(Task<HttpResponseMessage> postResponse)
+        {
+            return (JObject)JsonConvert.DeserializeObject(postResponse.Result.Content.ReadAsStringAsync().Result);
+        }
+
+
+        private Task<HttpResponseMessage> GetRequest(string requestedUri)
+        {
+            
+            var response = Task.Run(() => Client.GetAsync(requestedUri));
+            response.Wait();
+            return response;
+        }
+
+
+        private Task<HttpResponseMessage> PostRequest(string requestedUri, string payload)
+        {
+            HttpContent content = new StringContent(payload,
+                                        Encoding.UTF8, "application/json");
+
+
+            // Act
+            var response = Task.Run(() => Client.PostAsync(requestedUri, content));
+            response.Wait();
+            return response;
+        }
     }
 }
